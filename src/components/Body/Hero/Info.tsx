4bc0c06cccf24d17ec7a580719b3987f55248fed
uygarpolat <uygarpolat@gmail.com>
2026-02-02 03:29:34 +0200

    hero continued

--- src/components/Body/Hero/Info.tsx
+++ src/components/Body/Hero/Info.tsx
@@ -1,11 +1,33 @@
+import { motion } from "framer-motion";
+
 import "./Info.css";
 
 export default function Info() {
+  const animationSettings = {
+    initial: { opacity: 0, x: 100 },
+    animate: { opacity: 1, x: 0 },
+  };
+
   return (
     <div id="info">
-      <h2>Hi there, my name is</h2>
-      <h1>Uygar.</h1>
-      <p>I'm a software engineer based in Helsinki!</p>
+      <motion.h2
+        {...animationSettings}
+        transition={{ type: "spring", duration: 2, delay: 0.1 }}
+      >
+        Hi there, my name is
+      </motion.h2>
+      <motion.h1
+        {...animationSettings}
+        transition={{ type: "spring", duration: 2, delay: 0.2 }}
+      >
+        Uygar.
+      </motion.h1>
+      <motion.p
+        {...animationSettings}
+        transition={{ type: "spring", duration: 2, delay: 0.3 }}
+      >
+        I'm a software engineer based in Helsinki!
+      </motion.p>
     </div>
   );
 }
