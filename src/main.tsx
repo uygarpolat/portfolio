5972635406d745b235b8d2c975d1ff2eecf08da9
uygarpolat <uygarpolat@gmail.com>
2026-02-04 20:13:09 +0200

    vercel analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import "devicon/devicon.min.css";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+	<StrictMode>
+		<App />
+		<Analytics />
+	</StrictMode>
 );
